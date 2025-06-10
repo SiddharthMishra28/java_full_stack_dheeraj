e3d90b9ff5ded4df90d9d1d62d2666ae82244ba0
SiddharthMishra28 <connectwithsiddharthm@gmail.co>
2025-06-10 10:23:45 +0530

    Core Java Arrays - Procedural Programming Session 3

--- src/main/java/com/proceduralprogramming/assignments/ShoppingMartArrays.java
+++ src/main/java/com/proceduralprogramming/assignments/ShoppingMartArrays.java
@@ -5,7 +5,7 @@ public class ShoppingMartArrays {
         // DECLARE TWO DIMENSIONAL ARRAY FOR PRODUCT LIST
         // PRODUCT LIST CONTAINS - NAME OF PRODUCT, PRICE OF PRODUCT
         // SHOULD SHOW AVERAGE PRICE OF YOUR SHOPPING CART
-        // SHOW PRODUCT WITH HIGHEST PRICE
+        // SHOW PRODUCT WITH HIGHEST PRICE - hint requires 2d loops
         // SHOW PRODUCT WITH LOWEST PRICE
         // String[][] myCart = new String[take_from_user][2]
     }
